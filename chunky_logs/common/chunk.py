0f1cf86af2dd718cb0c129bd8e0274ef3c3a762c
James Bourne <james.a.bourne.85@gmail.com>
2025-02-06 14:36:53 +0000

    Adding initial code/test for chunk class

---
+++ chunky_logs/common/chunk.py
@@ -0,0 +1,42 @@
+import os
+import logging
+import pathlib
+from zipfile import ZipFile
+from chunky_logs.common.metadata import MetaData
+
+class Chunk:
+    CHUNK_FILE_EXTENSION = '.chunk'
+    def __init__(self, group_path: pathlib.Path, chunk_name: pathlib.Path):
+        self._logger = logging.getLogger(self.__class__.__name__)
+        self._base_path = group_path
+        self._file = chunk_name.with_suffix(Chunk.CHUNK_FILE_EXTENSION)
+
+        self.metadata = MetaData(self._file)
+
+        self._managed_files = [
+            self._file,
+            self.metadata.file
+        ]
+
+    def delete(self):
+        """
+        This deletes all the managed files associated with this Chunk
+        """
+        for file in self._managed_files:
+            if os.path.exists(file):
+                os.remove(file)
+                self._logger.debug(f"Removed Chunk managed file. file={file}")
+
+    def archive(self):
+        """
+        This archives all the managed files associated with this Chunk, all managed files will be zipped up and then
+        removed
+        """
+        archive_filename = self.get_associated_file('zip')
+        with ZipFile(archive_filename, 'w') as archive:
+            for file in self._managed_files:
+                if os.path.exists(file):
+                    self._logger.debug(f"Archiving Chunk file. archive={archive_filename} file={file}")
+                    archive.write(file)
+
+        self.delete()
---
+++ tests/common/test_chunk.py
@@ -0,0 +1,26 @@
+import tempfile
+from unittest import mock
+import pathlib
+import pytest
+from chunky_logs.common.chunk import Chunk
+
+@mock.patch('os.path.exists')
+@mock.patch("chunky_logs.common.chunk.MetaData")
+def test_constructor(mock_metadata, patch_os_path_exists):
+    patch_os_path_exists.return_value = True
+
+    base_path = pathlib.Path(tempfile.NamedTemporaryFile().name)
+    chunk_name = pathlib.Path('chunk_1')
+
+    mock_metadata_instance = mock.MagicMock()
+    mock_metadata.return_value = mock_metadata_instance
+    with mock.patch.object(mock_metadata_instance, "file", new_callable=mock.PropertyMock) as mock_property:
+        mock_property.return_value = 'metadata_file'
+
+        test_chunk = Chunk(base_path, chunk_name)
+
+        assert test_chunk._managed_files == [
+            chunk_name.with_suffix('.chunk'),
+            mock_property
+        ]
+
