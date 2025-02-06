b21c3a80dcb0644dc0ee50d2757e28207c124d67
James Bourne <james.a.bourne.85@gmail.com>
2025-02-06 14:58:03 +0000

    Removing the requirement for the tempfile as using PurePosixPath instead

--- tests/common/test_chunk.py
+++ tests/common/test_chunk.py
@@ -1,4 +1,3 @@
-import tempfile
 from unittest import mock
 import pathlib
 import pytest
@@ -6,31 +5,31 @@ from chunky_logs.common.chunk import Chunk, ChunkManagedFileError
 
 @mock.patch("chunky_logs.common.chunk.MetaData")
 def test_constructor(mock_metadata):
-    base_path = pathlib.Path(tempfile.NamedTemporaryFile().name)
-    chunk_name = pathlib.Path('chunk_1')
+    group_path = pathlib.PurePosixPath('/tmp/test_group')
+    chunk_name = pathlib.PurePosixPath('chunk_1')
 
     mock_metadata_instance = mock.MagicMock()
     mock_metadata.return_value = mock_metadata_instance
     with mock.patch.object(mock_metadata_instance, "file", new_callable=mock.PropertyMock) as mock_property:
         mock_property.return_value = 'chunk_1.metadata'
 
-        test_chunk = Chunk(base_path, chunk_name)
+        test_chunk = Chunk(group_path, chunk_name)
 
         assert test_chunk._managed_files == [
-            chunk_name.with_suffix('.chunk'),
+            pathlib.PurePosixPath('/tmp/test_group/chunk_1.chunk'),
             mock_property
         ]
 
 @mock.patch('os.remove')
 @mock.patch("chunky_logs.common.chunk.MetaData")
 def test_delete_success(mock_metadata, patch_os_remove):
-    base_path = pathlib.Path(tempfile.NamedTemporaryFile().name)
-    chunk_name = pathlib.Path('chunk_1')
+    group_path = pathlib.PurePosixPath('/tmp/test_group')
+    chunk_name = pathlib.PurePosixPath('chunk_1')
 
     mock_metadata_instance = mock.MagicMock()
     mock_metadata.return_value = mock_metadata_instance
 
-    test_chunk = Chunk(base_path, chunk_name)
+    test_chunk = Chunk(group_path, chunk_name)
     test_chunk._managed_files = [
         'managed_file_1',
         'managed_file_2',
@@ -47,13 +46,13 @@ def test_delete_success(mock_metadata, patch_os_remove):
 @mock.patch('os.remove')
 @mock.patch("chunky_logs.common.chunk.MetaData")
 def test_delete_failure(mock_metadata, patch_os_remove):
-    base_path = pathlib.Path(tempfile.NamedTemporaryFile().name)
-    chunk_name = pathlib.Path('chunk_1')
+    group_path = pathlib.PurePosixPath('/tmp/test_group')
+    chunk_name = pathlib.PurePosixPath('chunk_1')
 
     mock_metadata_instance = mock.MagicMock()
     mock_metadata.return_value = mock_metadata_instance
 
-    test_chunk = Chunk(base_path, chunk_name)
+    test_chunk = Chunk(group_path, chunk_name)
     test_chunk._managed_files = [
         'managed_file_1',
         'managed_file_2',
