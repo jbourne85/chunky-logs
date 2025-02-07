697f1fbb7a2a46008c58e5390d525dc7e4d31438
James Bourne <james.a.bourne.85@gmail.com>
2025-02-07 11:48:39 +0000

    Converting ParserChunk tests to read from real files, given the low level file IO that is being performed by this test there is a use case for greater benefit from using real files rather than mocking the IO ops.

--- tests/parser/test_parser_chunk.py
+++ tests/parser/test_parser_chunk.py
@@ -1,25 +1,32 @@
 import pathlib
 import pytest
+import shutil
+import tempfile
 from unittest import mock, TestCase
 from chunky_logs.parser.parser_chunk import ParserChunk
 
 class TestParserChunk(TestCase):
     @mock.patch("chunky_logs.common.chunk.MetaData")
     def setUp(self, mock_metadata):
-        group_path = pathlib.PurePosixPath('/tmp/test_group')
+        self.test_data_directory = tempfile.mkdtemp()
+        group_path = pathlib.PurePosixPath(self.test_data_directory)
         chunk_name = pathlib.PurePosixPath('chunk_1')
 
         self.mock_metadata_instance = mock.MagicMock()
         mock_metadata.return_value = self.mock_metadata_instance
 
-        self.metadata_file_patcher = mock.patch.object(self.mock_metadata_instance, "file", new_callable=mock.PropertyMock)
+        self.metadata_file_patcher = mock.patch.object(self.mock_metadata_instance, "file", new='chunk_1.metadata')
         self.metadata_file_property = self.metadata_file_patcher.start()
-        self.metadata_file_property.return_value = 'chunk_1.metadata'
 
         self.test_parser_chunk = ParserChunk(group_path, chunk_name)
 
+    def set_test_chunk_file_contents(self, file_contents):
+        with open(self.test_parser_chunk._chunk_file, 'w') as test_chunk_file:
+            test_chunk_file.writelines("\n".join(file_contents))
+
     def tearDown(self):
         self.metadata_file_patcher.stop()
+        shutil.rmtree(self.test_data_directory)
 
     def test_read_success(self):
         test_data_lines = [
@@ -29,36 +36,11 @@ class TestParserChunk(TestCase):
             "line4",
             "line5"
         ]
-        test_tells_pos = [
-            10,
-            20,
-            30,
-            40,
-            50
-        ]
-        expected_seeks = [
-            None,
-            10,
-            20,
-            30,
-            40,
-        ]
+        self.set_test_chunk_file_contents(test_data_lines)
 
-        mock_chunk_file = mock.mock_open()
-        with mock.patch('builtins.open', mock_chunk_file):
-            for line_data, tell_pos, expected_seek_pos in zip(test_data_lines, test_tells_pos, expected_seeks):
-                mock_chunk_file.return_value.readline.return_value = line_data
-                mock_chunk_file.return_value.tell.return_value = tell_pos
-
-                # Check that the correct line is read, and that the position in the chunk is updated
-                assert line_data == self.test_parser_chunk.read_line()
-                assert tell_pos == self.test_parser_chunk._chunk_pos
-
-                # Check that the seek is called starting after the first call
-                if expected_seek_pos:
-                    mock_chunk_file().seek.assert_called_with(expected_seek_pos)
-                else:
-                    mock_chunk_file().seek.assert_not_called()
+        for line_data in test_data_lines:
+            # Check that the correct line is read, and that the position in the chunk is updated
+            assert line_data == self.test_parser_chunk.read_line()
 
     def test_has_changed(self):
         with mock.patch.object(self.mock_metadata_instance, "checksum", new='9256f72dec56351070913a92666bd6ad'):
@@ -81,11 +63,9 @@ class TestParserChunk(TestCase):
             "line4",
             "line5"
         ]
+        self.set_test_chunk_file_contents(test_data_lines)
 
-        mocked_chunk_file = mock.mock_open(read_data="\n".join(test_data_lines))
-        with mock.patch("builtins.open", mocked_chunk_file):
-            with mock.patch.object(self.mock_metadata_instance, "chunk_line_count", new=5):
-
-                self.assertEquals('line1', self.test_parser_chunk.head())
-                self.assertListEqual(test_data_lines[0:3], self.test_parser_chunk.head(3))
-                self.assertListEqual(test_data_lines[0:], self.test_parser_chunk.head(100))
+        with mock.patch.object(self.mock_metadata_instance, "chunk_line_count", new=5):
+            self.assertEquals('line1', self.test_parser_chunk.head())
+            self.assertListEqual(test_data_lines[0:3], self.test_parser_chunk.head(3))
+            self.assertListEqual(test_data_lines[0:], self.test_parser_chunk.head(100))
