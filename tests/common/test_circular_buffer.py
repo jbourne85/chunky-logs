caad902125a418bbabbca46f9ae00aeed7b90dde
James Bourne <james.a.bourne.85@gmail.com>
2025-02-10 14:00:46 +0000

    Adding test code for testing the circular buffer functionality

--- tests/common/test_circular_buffer.py
+++ tests/common/test_circular_buffer.py
@@ -21,3 +21,33 @@ def test_construct():
 
     with pytest.raises(CircularBufferIndexError):
         buffer[0] = 1
+
+def test_circular_buffer():
+    capacity = 10
+    buffer = CircularBuffer(capacity)
+
+    for i in range(1, 11):  # Add 1 -> 9 into the buffer (filling it)
+        buffer.push(i)
+
+    # Assert the buffer status
+    assert buffer.length() == capacity
+    assert buffer.capacity() == capacity
+    assert buffer.head() == 1
+    assert buffer.tail() == capacity
+    assert buffer.is_full() is True
+
+    for i in range(1, 11): # Assert the values in the buffer
+        assert buffer[i - 1] == i
+
+    for i in range(11, 21):  # Add 11 -> 20 into the buffer (filling it again)
+        buffer.push(i)
+
+    # Assert the buffer status
+    assert buffer.length() == capacity
+    assert buffer.capacity() == capacity
+    assert buffer.head() == 11
+    assert buffer.tail() == 20
+    assert buffer.is_full() is True
+
+    for i in range(1, 11): # Assert the values in the buffer
+        assert buffer[i - 1] == i + capacity
